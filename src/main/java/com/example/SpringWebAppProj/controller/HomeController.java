947dbb5547ed85d54521afa77b1cea210e7f8baf
MahabirGupta <mahabirg81@gmail.com>
2024-08-08 10:58:57 +0800

    Add HttpServletRequest and HttpSession and to send the name variable and retrive the name variable

--- src/main/java/com/example/SpringWebAppProj/controller/HomeController.java
+++ src/main/java/com/example/SpringWebAppProj/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.example.SpringWebAppProj.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,8 +10,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HomeController {
 
     @RequestMapping("home")
-    public String homePage(){
-        System.out.println("Hi"); //printing on console
-        return "home";
+    public String homePage(HttpServletRequest httpServletRequest){ // send a request
+
+        HttpSession httpSession = httpServletRequest.getSession();
+        String name = httpServletRequest.getParameter("name"); // to get the name parameter
+        System.out.println("Hi " + name); //printing on console and verfiy is the name coming back
+        httpSession.setAttribute("name",name);
+        return "home"; //don't put .jsp at the end
     }
 }

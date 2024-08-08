3088813285d8f491571dbf26c6a0dbb90033adf7
MahabirGupta <mahabirg81@gmail.com>
2024-08-08 09:45:11 +0800

    first commit

---
+++ src/main/java/com/example/SpringWebAppProj/SpringWebAppProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringWebAppProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebAppProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebAppProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringWebAppProj/SpringWebAppProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringWebAppProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebAppProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
